29f86e797ee0565bef9cdb6be7616abcb1bb53a5
Julia Halvdansson Lonn <julia.halvdansson.lonn@iths.se>
2023-01-13 09:24:08 +0100

    Remove

--- app/src/main/java/se/iths/Artist.java
+++ app/src/main/java/se/iths/Artist.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 public class Artist {
     private final long ArtistId;
     private String Name;
-    private Collection<Album> albums = new ArrayList<>();
 
     public Artist(long id, String name){
         this.ArtistId = id;
@@ -25,9 +24,6 @@ public class Artist {
         this.Name = name;
     }
 
-    public void add(Album album){
-        albums.add(album);
-    }
 
     public String toString(){
         return "Artist: " + ArtistId + " " + Name + " - ";
